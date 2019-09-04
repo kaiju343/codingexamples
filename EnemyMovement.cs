6b47e9ec1baa933a232a9a8f5d651bd20d0661ff
kaiju343 <54901357+kaiju343@users.noreply.github.com>
2019-09-04 17:10:00 +0200

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed;
+
+	private EnemyMovementRail myRail;
+
+	private int currentSeg;
+	private float transition;
+	private bool isCompleted;
+
+	public bool Catmull = false;
+
+	void OnEnable()
+	{
+		myRail = gameObject.GetComponentInChildren<EnemyMovementRail> ();
+		currentSeg = 0;
+		transition = 0;
+		isCompleted = false;
+	}
+
+	public void CompletedRail()
+	{
+		isCompleted = true;
+	}
+	
+	void FixedUpdate () {
+		
+		if (isCompleted)
+			gameObject.SetActive (false);
+		
+		if (!myRail)
+			return;
+		if (!isCompleted)
+			Move ();
+	}
+
+	float yTilt;
+	float xDelta;
+	float smoothVelo;
+	private void Move()
+	{
+		transition += Time.deltaTime * 1 / speed;
+
+		if (transition > 1) {
+			transition = 0;
+			currentSeg++;
+		} else if (transition<0) {
+			transition = 1;
+			currentSeg--;
+		}
+
+
+		if (!Catmull)
+			transform.position = myRail.LinearPosition (currentSeg, transition, gameObject.GetComponent<EnemyMovement> ());
+		else
+			transform.position = myRail.CatmullPosition (currentSeg, transition,gameObject.GetComponent<EnemyMovement>());
+
+		// check shoot
+
+
+		if (xDelta > transform.position.x)
+			yTilt = 12.5f;
+		else if (xDelta < transform.position.x)
+			yTilt = -12.5f;
+		else
+			yTilt = 0;
+
+
+		yTilt = Mathf.SmoothDampAngle(gameObject.transform.rotation.eulerAngles.y, yTilt, ref smoothVelo,0.3f);
+
+		transform.rotation = Quaternion.Euler (0,yTilt, 0);
+		
+		xDelta = transform.position.x;
+	}
+}
---
+++ EnemyMovementRail.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementRail : MonoBehaviour {
+	public Transform[] nodes;
+	private Vector3[] nodesPos;
+	public bool mirrored = false;
+
+	// Use this for initialization
+	void Start () {
+		
+		nodes = gameObject.GetComponentsInChildren<Transform>();
+
+		nodesPos = new Vector3[nodes.Length];
+		for (int i = 0; i < nodes.Length; i++) {
+			nodesPos [i] = nodes [i].position;
+		}
+
+		if (mirrored)
+			MirrorTracks ();
+	}
+
+	void OnEnable()
+	{
+		if (mirrored)
+			MirrorTracks ();
+	}
+
+	public void MirrorTracks()
+	{
+		for (int i = 0; i < nodesPos.Length; i++) {
+			nodesPos [i] = new Vector3 (nodesPos [i].x * -1, nodesPos [i].y, nodesPos [i].z);
+		}
+	}
+	
+	public Vector3 LinearPosition(int seg,float ratio, EnemyMovement enemy){
+		
+		if (seg == nodesPos.Length - 1) {
+			enemy.CompletedRail ();
+			return new Vector3(0,-25,0);
+		}
+
+		if (nodes [seg].tag == "EnemyAttack")
+			enemy.gameObject.GetComponent<Enemy> ().EnemyShoot (seg);
+	
+		Vector3 p1 = nodesPos [seg];
+		Vector3 p2 = nodesPos [seg + 1];
+		return Vector3.Lerp (p1, p2, ratio);
+	}
+		
+	public Vector3 CatmullPosition(int seg,float ratio, EnemyMovement enemy){
+		if (seg == nodesPos.Length - 1) {
+			enemy.CompletedRail ();
+			return new Vector3(0,-25,0);
+		}
+
+		if (nodes [seg].tag == "EnemyAttack")
+			enemy.gameObject.GetComponent<Enemy> ().EnemyShoot (seg);
+
+		Vector3 p1, p2, p3, p4;
+		if (seg == 0) {
+			p1 = nodesPos [seg];
+			p2 = p1;
+			p3 = nodesPos [seg + 1];
+			p4 = nodesPos [seg + 2];
+		} else if (seg == nodes.Length - 2) {
+			p1 = nodesPos [seg - 1];
+			p2 = nodesPos [seg];
+			p3 = nodesPos [seg + 1];
+			p4 = p3;
+		} else {
+			p1 = nodesPos [seg - 1];
+			p2 = nodesPos [seg];
+			p3 = nodesPos [seg + 1];
+			p4 = nodesPos [seg + 2];
+		}
+		float t2 = ratio * ratio;
+		float t3 = t2 * ratio;
+		float x = 0.5f * ((2.0f * p2.x) 
+			+ (-p1.x + p3.x) 
+			* ratio + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) 
+			* t2 + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) 
+			* t3);
+		float y = 0.5f * ((2.0f * p2.y) 
+			+ (-p1.y + p3.y) 
+			* ratio + (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y) 
+			* t2 + (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) 
+			* t3);
+		return new Vector3 (x, y, 0);
+	}
+}
